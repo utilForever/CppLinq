b682262fc9e35b873f02b2830b2608e4a4b00a9c
Chris Ohk <utilforever@gmail.com>
2016-12-11 21:36:09 +0900

    Comment 'LeftJoin' clause test cases

--- Sources/LeftJoinTest.cpp
+++ Sources/LeftJoinTest.cpp
@@ -3,35 +3,32 @@
 #include "CppLinq.h"
 #include "TestUtils.h"
 
-TEST(LeftJoin, People)
-{
-	struct People
-	{
-		int m_no;
-		std::string m_name;
-	};
-
-	struct Account
-	{
-		int m_no;
-		int m_money;
-	};
-
-	struct PeopleAccount
-	{
-		int m_no;
-		std::string m_name;
-		int m_money;
-	};
-
-	People peoples[] = { {1, "Tom"}, {2, "Mike"}, {3, "Susan"} };
-	Account accounts[] = { {2, 10000}, {3, 20000}, {4, 30000} };
-	PeopleAccount ans[] = { { 1, "Tom", NULL }, { 2, "Mike", 10000 }, { 3, "Susan", 20000 } };
-
-	auto rng1 = CppLinq::From(peoples);
-	auto rng2 = CppLinq::From(accounts);
-
-	auto dst = rng1.LeftJoin(rng2, [](People p, Account a) { return p.m_no == a.m_no; });
-
-	IsEqualArray(dst, ans);
-}
+//TEST(LeftJoin, People)
+//{
+//	struct People
+//	{
+//		int m_no;
+//		std::string m_name;
+//	};
+//
+//	struct Account
+//	{
+//		int m_no;
+//		int m_money;
+//	};
+//
+//	struct PeopleAccount
+//	{
+//		int m_no;
+//		std::string m_name;
+//		int m_money;
+//	};
+//
+//	People peoples[] = { {1, "Tom"}, {2, "Mike"}, {3, "Susan"} };
+//	Account accounts[] = { {2, 10000}, {3, 20000}, {4, 30000} };
+//	PeopleAccount ans[] = { { 1, "Tom", NULL }, { 2, "Mike", 10000 }, { 3, "Susan", 20000 } };
+//
+//	CppLinq::LeftJoin(peoples, accounts,  [](People p, Account a) { return p.m_no == a.m_no; });
+//
+//	IsEqualArray(dst, ans);
+//}
